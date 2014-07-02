89da3084700403c140c116096d5ad9c3439abf91
Zhe Li <linuxcity.jn@gmail.com>
2014-07-02 10:01:04 +0200

    added mag declination for sweden

--- config.h
+++ config.h
@@ -690,7 +690,8 @@
        Convert the degree+minutes into decimal degree by ==> degree+minutes*(1/60)
        Note the sign on declination it could be negative or positive (WEST or EAST) */
     //#define MAG_DECLINATION  3.96f              //For Budapest Hungary.
-    #define MAG_DECLINATION  0.0f   //(**)
+    #define MAG_DECLINATION  5.37f              //For Gothenburg Sweden.    
+    //#define MAG_DECLINATION  0.0f   //(**)
 
     #define GPS_LEAD_FILTER                      // Adds a forward predictive filterig to compensate gps lag. Code based on Jason Short's lead filter implementation
     
